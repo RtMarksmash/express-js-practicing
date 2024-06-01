744191434bd5d5474c2e02bc812bc8021f2bbd95
mario mora <jorgemariomoramolina@gmail.com>
2024-06-01 01:13:29 -0500

    modeling to page

--- routes/admin.js
+++ routes/admin.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const path = require('path');
-const router = express.Router();
+const router = express.Router()
 const rootPath = require('../util/path');
+
 
 router.get('/add-products', (req, res) => {
     res.sendFile(path.join(rootPath, 'views', 'add-product.html'))
 });
+
 
 router.post('/add-products', (req, res) => {
     console.log(req.body)
